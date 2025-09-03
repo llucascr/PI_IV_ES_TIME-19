0078ad42094722cc49dbb998c33937ba525f4dfa
llucascr <lucasdecamposranzani@gmail.com>
2025-09-03 17:04:08 -0300

    feat: Padrão de Arquitetura

---
+++ backend/src/main/java/com/puc/PI4/Software/Morango/Application.java
@@ -0,0 +1,13 @@
+package com.puc.PI4.Software.Morango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/puc/PI4/Software/Morango/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puc.PI4.Software.Morango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
