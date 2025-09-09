e84be3dd088994cab1b87b683f58d01974e78e15
llucascr <lucasdecamposranzani@gmail.com>
2025-09-09 20:32:34 -0300

    feat: endpoint teste conexão ao mongodb

---
+++ backend/src/main/java/com/puc/PI4/Software/Morango/models/mongodb/Praga.java
@@ -0,0 +1,22 @@
+package com.puc.PI4.Software.Morango.models.mongodb;
+
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "praga")
+public class Praga {
+
+    @Id
+    private String id;
+
+    private String nomeComum;
+
+    private String nomeCientifico;
+
+}
