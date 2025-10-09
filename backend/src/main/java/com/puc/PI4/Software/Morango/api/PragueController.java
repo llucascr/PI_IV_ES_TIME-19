b4f541c5a74ec281a75834809127bbeffb394075
caiosc1 <160679697+caiodscunha@users.noreply.github.com>
2025-10-09 11:27:50 -0300

    feat: implemented updatePrague endPoint

--- backend/src/main/java/com/puc/PI4/Software/Morango/api/PragueController.java
+++ backend/src/main/java/com/puc/PI4/Software/Morango/api/PragueController.java
@@ -1,17 +1,18 @@
 package com.puc.PI4.Software.Morango.api;
 
 import com.puc.PI4.Software.Morango.dto.request.prague.PragueRequest;
+import com.puc.PI4.Software.Morango.dto.response.Post.PostResponse;
 import com.puc.PI4.Software.Morango.dto.response.prague.PragueResponse;
 import com.puc.PI4.Software.Morango.models.Prague;
 import com.puc.PI4.Software.Morango.repositories.PragueRepository;
 import com.puc.PI4.Software.Morango.services.PragueService;
+import jdk.jshell.spi.ExecutionControl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,6 +24,19 @@ public class PragueController {
     @PostMapping(value = "/create")
     public ResponseEntity<PragueResponse> createPrague(@RequestBody PragueRequest pragueRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(pragueService.createPrague(pragueRequest));
+    }
+
+    @GetMapping(value = "/list")
+    public ResponseEntity<List<PragueResponse>> listAllPragues(
+            @RequestParam(defaultValue = "0", required = false) int page,
+            @RequestParam(defaultValue = "10", required = false) int numberOfPragues
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(pragueService.listAllPragues(page, numberOfPragues).getContent());
+    }
+
+    @PutMapping(value = "/update")
+    public ResponseEntity<PragueResponse> updatePrague(@RequestBody PragueRequest pragueRequest, @RequestParam String pragueId) {
+        return ResponseEntity.status(HttpStatus.OK).body(pragueService.updatePrague(pragueRequest, pragueId));
     }
 
 }
