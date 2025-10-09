a69c101efa8a3fb94636081bcb988c9123d04215
caiosc1 <160679697+caiodscunha@users.noreply.github.com>
2025-10-09 11:27:50 -0300

    feat: implemented updatePrague endPoint

--- backend/src/main/java/com/puc/PI4/Software/Morango/services/PragueService.java
+++ backend/src/main/java/com/puc/PI4/Software/Morango/services/PragueService.java
@@ -3,11 +3,13 @@ package com.puc.PI4.Software.Morango.services;
 import com.puc.PI4.Software.Morango.dto.request.prague.PragueRequest;
 import com.puc.PI4.Software.Morango.dto.response.prague.PragueResponse;
 import com.puc.PI4.Software.Morango.exceptions.prague.PragueAlreadyExists;
+import com.puc.PI4.Software.Morango.exceptions.prague.PragueNotFound;
 import com.puc.PI4.Software.Morango.models.Prague;
 import com.puc.PI4.Software.Morango.repositories.PragueRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 
@@ -32,4 +34,21 @@ public class PragueService {
 
         return modelMapper.map(pragueRepository.save(prague), PragueResponse.class);
     }
+
+    public PragueResponse updatePrague(PragueRequest pragueRequest, String pragueId) {
+        Prague prague = pragueRepository.findById(pragueId).orElseThrow(
+                () -> new PragueNotFound("Prague with id " + pragueId + " not found")
+        );
+
+        Prague updatedPrage = Prague.builder()
+                .id(pragueId)
+                .cientificName(pragueRequest.getCientificName())
+                .comumName(pragueRequest.getComumName())
+                .build();
+
+        return modelMapper.map(pragueRepository.save(prague), PragueResponse.class);
+    }
+
+    public Page<PragueResponse> listAllPragues(int page, int numberOfPragues)
+    {}
 }
