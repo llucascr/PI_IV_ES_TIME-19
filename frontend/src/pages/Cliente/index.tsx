dcbae175db958bb8f11c1a772a755b861b37f4a0
Fillipe Faria <fillipe.sf@puccampinas.edu.br>
2025-11-12 08:17:14 -0300

    fix

--- frontend/src/pages/Cliente/index.tsx
+++ frontend/src/pages/Cliente/index.tsx
@@ -70,6 +70,7 @@ export function Cliente() {
   return (
     <div className="flex h-full">
       {/* Coluna esquerda: lista de clientes */}
+      <div className="flex mt"></div>
       <div className="flex-1 p-5 overflow-y">
         <div className="">
           <div className="mb-5 flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 ring-1 ring-gray-200">
