d8b813eb2718b0fddf4fea79614f03cc6396f7cb
Fillipe Faria <fillipe.sf@puccampinas.edu.br>
2025-11-20 23:13:36 -0300

    Update CPFFormatRequest.java

--- ServidorJava/src/main/java/org/servidor/request/CPFFormatRequest.java
+++ ServidorJava/src/main/java/org/servidor/request/CPFFormatRequest.java
@@ -14,6 +14,23 @@ public class CPFFormatRequest implements Command {
         return this.cpf;
     }
 
+    public String formatCPF() {
+        if (cpf == null) {
+            return null;
+        }
+
+        String cpfDigits = cpf.replaceAll("\\D", "");
+
+        if (cpfDigits.length() != 11) {
+            return cpf;
+        }
+
+        return cpfDigits.substring(0, 3) + "." +
+                cpfDigits.substring(3, 6) + "." +
+                cpfDigits.substring(6, 9) + "-" +
+                cpfDigits.substring(9, 11);
+    }
+
     @Override
     public String toString() {
         return "Formatar CPF: " + this.cpf;
