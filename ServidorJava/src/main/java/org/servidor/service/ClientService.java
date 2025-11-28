92a82845ce2d98122f1afa1aab5084c771a8319b
Fillipe Faria <fillipe.sf@puccampinas.edu.br>
2025-11-28 19:22:38 -0300

    feat CPFFormat

--- ServidorJava/src/main/java/org/servidor/service/ClientService.java
+++ ServidorJava/src/main/java/org/servidor/service/ClientService.java
@@ -3,6 +3,7 @@ package org.servidor.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mindrot.jbcrypt.BCrypt;
+import org.servidor.utility.CPFFormat;
 import org.servidor.utility.EmailValidation;
 import org.servidor.utility.CNPJFormat;
 import org.servidor.utility.PasswordEncryption;
@@ -64,78 +65,14 @@ public class ClientService {
     }
 
     public String formatarCPF(String cpf) throws JsonProcessingException {
-        // Remover caracteres não numéricos
-        String cpfLimpo = cpf.replaceAll("[^0-9]", "");
-
-        // Validar se tem 11 dígitos
-        if (cpfLimpo.length() != 11) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("tipo", "validarCPF");
-            response.put("valido", false);
-            response.put("erro", "CPF deve conter exatamente 11 dígitos");
-            return new ObjectMapper().writeValueAsString(response);
+        boolean isValido = CPFFormat.isValid(cpf);
+        String resposta = "-1";
+        if (isValido) {
+            resposta = CPFFormat.formatarCPF(cpf);
         }
-
-        // Validar se não é sequência de números iguais
-        if (cpfLimpo.matches("(\\d)\\1{10}")) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("tipo", "validarCPF");
-            response.put("valido", false);
-            response.put("erro", "CPF não pode ser formado por dígitos repetidos");
-            return new ObjectMapper().writeValueAsString(response);
-        }
-
-        // Validar dígitos verificadores
-        if (!validarDigitosVerificadores(cpfLimpo)) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("tipo", "validarCPF");
-            response.put("valido", false);
-            response.put("erro", "Dígitos verificadores inválidos");
-            return new ObjectMapper().writeValueAsString(response);
-        }
-
-        // Formatar CPF (XXX.XXX.XXX-XX)
-        String cpfFormatado = cpfLimpo.replaceAll(
-                "(\\d{3})(\\d{3})(\\d{3})(\\d{2})",
-                "$1.$2.$3-$4"
-        );
-
-        // Retornar resposta formatada
         Map<String, Object> response = new HashMap<>();
-        response.put("tipo", "validarCPF");
-        response.put("valido", true);
-        response.put("cpf", cpfFormatado);
-        response.put("cpfSemFormatacao", cpfLimpo);
-
+        response.put("tipo", "formatarCPF");
+        response.put("formatado", resposta);
         return new ObjectMapper().writeValueAsString(response);
     }
-
-    /**
-     * Valida os dígitos verificadores do CPF
-     * Algoritmo conforme normas da Receita Federal do Brasil[79]
-     */
-    private boolean validarDigitosVerificadores(String cpf) {
-        // Calcular primeiro dígito verificador
-        int soma = 0;
-        for (int i = 0; i < 9; i++) {
-            soma += (cpf.charAt(i) - '0') * (10 - i);
-        }
-
-        int resto = soma % 11;
-        int primeiroDigito = (resto < 2) ? 0 : (11 - resto);
-
-        // Calcular segundo dígito verificador
-        soma = 0;
-        for (int i = 0; i < 10; i++) {
-            soma += (cpf.charAt(i) - '0') * (11 - i);
-        }
-
-        resto = soma % 11;
-        int segundoDigito = (resto < 2) ? 0 : (11 - resto);
-
-        // Validar se os dígitos calculados correspondem aos informados
-        return (primeiroDigito == (cpf.charAt(9) - '0')) &&
-                (segundoDigito == (cpf.charAt(10) - '0'));
-    }
-
 }
